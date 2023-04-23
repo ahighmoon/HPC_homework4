02069c6523651fd1e5609282570c4f7452048274
Yifei <yz6994@nyu.edu>
2023-04-22 23:35:15 -0400

    fix bugs

--- mpi-scan.cpp
+++ mpi-scan.cpp
@@ -16,43 +16,43 @@ void scan_seq(long* prefix_sum, const long* A, long n) {
     }
 }
 
-void scan_omp(long* prefix_sum, const long* A, long n) {
-    //int p = omp_get_num_threads();
-    int p = 8;
-    //int t = omp_get_thread_num();
-    // Fill out parallel scan: One way to do this is array into p chunks
-    // Do a scan in parallel on each chunk, then share/compute the offset
-    // through a shared vector and update each chunk by adding the offset
-    // in parallel
+// void scan_omp(long* prefix_sum, const long* A, long n) {
+//     //int p = omp_get_num_threads();
+//     int p = 8;
+//     //int t = omp_get_thread_num();
+//     // Fill out parallel scan: One way to do this is array into p chunks
+//     // Do a scan in parallel on each chunk, then share/compute the offset
+//     // through a shared vector and update each chunk by adding the offset
+//     // in parallel
     
-    // size of each chunk should be a long int, rounded up
+//     // size of each chunk should be a long int, rounded up
 
-    #pragma omp parallel for num_threads(p)
-    for (int i = 0; i < p; i++){
-        long head = i * chunksize;
-        prefix_sum[head] = A[head]; // I change this code to include the 1st entry of A
-        for (int j = head + 1; j < std::min(head + chunksize, n); j++){
-            prefix_sum[j] = prefix_sum[j - 1] + A[j]; // change here from A[j-1] to A[j] to include current position in A
-        }
-    }
+//     #pragma omp parallel for num_threads(p)
+//     for (int i = 0; i < p; i++){
+//         long head = i * chunksize;
+//         prefix_sum[head] = A[head]; // I change this code to include the 1st entry of A
+//         for (int j = head + 1; j < std::min(head + chunksize, n); j++){
+//             prefix_sum[j] = prefix_sum[j - 1] + A[j]; // change here from A[j-1] to A[j] to include current position in A
+//         }
+//     }
     
-    // compute the offset array serially
-    long* correction = (long*) malloc((p-1) * sizeof(long));
-    correction[0] = 0;
-    for (int i = 1; i < p; i++) {
-        correction[i] = correction[i-1] + prefix_sum[i * chunksize - 1];
-    }
+//     // compute the offset array serially
+//     long* correction = (long*) malloc((p-1) * sizeof(long));
+//     correction[0] = 0;
+//     for (int i = 1; i < p; i++) {
+//         correction[i] = correction[i-1] + prefix_sum[i * chunksize - 1];
+//     }
 
-    // use the offset array to update scanned result, parallel
-    for (int i = 1; i < p; i++){
-        long head = i * chunksize;
-        #pragma omp parallel for num_threads(p)
-        for (int j = head; j < std::min(head + chunksize, n); j++){
-            prefix_sum[j] += correction[i];
-        }
-    }
-    free(correction);
-}
+//     // use the offset array to update scanned result, parallel
+//     for (int i = 1; i < p; i++){
+//         long head = i * chunksize;
+//         #pragma omp parallel for num_threads(p)
+//         for (int j = head; j < std::min(head + chunksize, n); j++){
+//             prefix_sum[j] += correction[i];
+//         }
+//     }
+//     free(correction);
+// }
 
 int main(int argc, char* argv[]) {
     MPI_Init(&argc, &argv);
@@ -66,20 +66,21 @@ int main(int argc, char* argv[]) {
     
     long* A = (long*) malloc(N * sizeof(long));
     long* B1 = (long*) malloc(chunksize * sizeof(long));
-
+    for (long i = 0; i < chunksize; i++) B1[i] = 0;
+    
+    double tt; 
     if (rank == 0){
         long* B0 = (long*) malloc(N * sizeof(long));
         for (long i = 0; i < N; i++) A[i] = rand();
         for (long i = 0; i < N; i++) B0[i] = 0;
-        for (long i = 0; i < N; i++) B1[i] = 0;
         
-        double tt = MPI_Wtime(); ;
+        tt = MPI_Wtime(); ;
         scan_seq(B0, A, N);
-        printf("sequential-scan = %fs\n", MPI_Wtime();  - tt);
+        printf("sequential-scan = %fs\n", MPI_Wtime() - tt);
         free(B0);
+        tt = MPI_Wtime();
     }
 
-    tt = MPI_Wtime();
     MPI_Scatter(A, chunksize, MPI_LONG, B1, chunksize, MPI_LONG, 0, comm);
     //scan_omp(B1, A, N);
     //printf("parallel-scan   = %fs\n", MPI_Wtime() - tt);
